e2277bb973dc7910d9980bdad515dccff23b4ae4
Dirtyhabitsonline <dirtyhabitscasino@icloud.com>
2024-05-13 03:48:29 -0500

    config

---
+++ test build/betav2/src/main/java/com/dirtyhabits/betav2/Betav2Application.java
@@ -0,0 +1,13 @@
+package com.dirtyhabits.betav2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Betav2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Betav2Application.class, args);
+	}
+
+}
